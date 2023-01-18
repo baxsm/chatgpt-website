390d3a8062c8898065683a5a481535833dd388ab
Bobby aleem <bobbybeatmaker@hotmail.com>
2023-01-19 04:00:00 +0500

    production update

--- client/script.js
+++ client/script.js
@@ -71,7 +71,7 @@ const handleSubmit = async (e) => {
     loader(messageDiv);
 
     // fetch data from api
-    const response = await fetch('http://localhost:5000', {
+    const response = await fetch('https://chatgpt-backend.vercel.app', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
