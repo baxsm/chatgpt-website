5680933391074b61d218ad0e896f760b149d1e58
Bobby Aleem <bobbybeatmaker@hotmail.com>
2024-05-30 23:36:58 +0500

    update

--- server/server.js
+++ server/server.js
@@ -1,44 +1,45 @@
-import express from 'express'
-import * as dotenv from 'dotenv'
-import cors from 'cors'
-import { Configuration, OpenAIApi } from 'openai'
+import express from "express";
+import * as dotenv from "dotenv";
+import cors from "cors";
+import OpenAI from "openai";
 
 dotenv.config();
 
-const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
-})
-
-const openai = new OpenAIApi(configuration);
+const openai = new OpenAI({
+  baseURL: "https://openrouter.ai/api/v1",
+  apiKey: process.env.OPENAI_API_KEY,
+});
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/', async (req, res) => {
+app.get("/", async (req, res) => {
+  res.status(200).send({
+    message: "Working!",
+  });
+});
+
+app.post("/", async (req, res) => {
+  try {
+    const prompt = req.body.prompt;
+    const response = await openai.chat.completions.create({
+      model: "openchat/openchat-7b:free",
+      prompt: `${prompt}`,
+      temperature: 0.4,
+      max_tokens: 3000,
+      top_p: 1,
+      frequency_penalty: 0.5,
+      presence_penalty: 0,
+    });
     res.status(200).send({
-        message: 'Working!'
-    })
-})
+      bot: response.data.choices[0].text,
+    });
+  } catch (error) {
+    res.status(500).send({ error });
+  }
+});
 
-app.post('/', async (req, res) => {
-    try {
-        const prompt = req.body.prompt;
-        const response = await openai.createCompletion({
-            model: 'text-davinci-003',
-            prompt: `${prompt}`,
-            temperature: 0.4,
-            max_tokens: 3000,
-            top_p: 1,
-            frequency_penalty: 0.5,
-            presence_penalty: 0
-        })
-        res.status(200).send({
-            bot: response.data.choices[0].text
-        })
-    } catch (error) {
-        res.status(500).send({ error })
-    }
-})
-
-app.listen(5000, () => console.log('Server is running on port: http://localhost:5000'))
+app.listen(5000, () =>
+  console.log("Server is running on port: http://localhost:5000")
+);
